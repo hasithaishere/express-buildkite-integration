bb424e70b0d229705de85560145b4482174b6e3f
Hasitha Prabhath Gamage <hasithaishere@users.noreply.github.com>
2022-12-16 10:24:12 +0800

    Update execute-changeset.sh

--- .buildkite/scripts/execute-changeset.sh
+++ .buildkite/scripts/execute-changeset.sh
@@ -4,3 +4,10 @@ EXECUTION_STATUS=$(buildkite-agent meta-data get "cf-execution-state")
 CS_ARN=$(buildkite-agent meta-data get "${SERVICE_NAME}-cs-arn")
 echo $EXECUTION_STATUS
 echo $CS_ARN
+
+if [ "$EXECUTION_STATUS" = "yes" ]; then
+  echo "It's Yes"
+then
+  echo "User abort the chnages set execution"
+  exit 0
+fi
