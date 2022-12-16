5e21f1ea055861a1d3b73a8c6f535e9c8e9a5938
Hasitha Prabhath Gamage <hasithaishere@users.noreply.github.com>
2022-12-16 10:07:02 +0800

    Delete execute-change-set.sh

--- .buildkite/scripts/execute-change-set.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SERVICE_NAME="sample-integration-service"
-EXECUTION_STATUS=$(buildkite-agent meta-data get \"cf-execution-state\")
-CS_ARN=$(buildkite-agent meta-data get \"${SERVICE_NAME}-cs-arn\")
-echo $EXECUTION_STATUS
-echo $CS_ARN
