60141ccf668bf4bf9c92e22ec13308f0581483d8
Hasitha Prabhath Gamage <hasithaishere@users.noreply.github.com>
2022-12-16 10:16:03 +0800

    Update execute-changeset.sh

--- .buildkite/scripts/execute-changeset.sh
+++ .buildkite/scripts/execute-changeset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SERVICE_NAME="sample-integration-service"
-EXECUTION_STATUS=$(buildkite-agent meta-data get \"cf-execution-state\")
-CS_ARN=$(buildkite-agent meta-data get \"${SERVICE_NAME}-cs-arn\")
+EXECUTION_STATUS=$(buildkite-agent meta-data get "cf-execution-state")
+CS_ARN=$(buildkite-agent meta-data get "${SERVICE_NAME}-cs-arn")
 echo $EXECUTION_STATUS
 echo $CS_ARN
