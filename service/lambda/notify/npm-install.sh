876d4920fd01e506a94fd30b28424c774bca5cd7
Hasitha Prabhath Gamage <hasitha.hpmax@gmail.com>
2023-02-23 16:10:52 +0800

    Update version of IaC plugin

--- service/lambda/notify/npm-install.sh
+++ service/lambda/notify/npm-install.sh
@@ -1,1 +1,3 @@
-npm install --production
+find . -name package.json  \( -not -path "*/node_modules/*" \
+  -or -path "*/nodejs/node_modules/app/package.json" \) \
+  -exec bash -c "npm --prefix \$(dirname {}) install --production --no-audit" \;
