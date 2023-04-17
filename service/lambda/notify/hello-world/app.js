219279e53151297907e8b261202b85bcc7d78150
Hasitha Prabhath Gamage <hasitha.hpmax@gmail.com>
2023-04-17 13:55:56 +0800

    Update error and version

--- service/lambda/notify/hello-world/app.js
+++ service/lambda/notify/hello-world/app.js
@@ -11,11 +11,11 @@ function randomInteger(min, max) {
 
 const lambdaHandler = async (event, context) => {
     // const ret = await axios(url);
-    const randNo = randomInteger(10, 8600);
-    if (randNo < 2650) {
-        throw new Error(`New Error Less 2650: ${randNo}`);
-    } else if (randNo <  4650) {
-        throw new Error(`New Error Less 4650: ${randNo}`);
+    const randNo = randomInteger(10, 8700);
+    if (randNo < 2700) {
+        throw new Error(`New Error Less 2700: ${randNo}`);
+    } else if (randNo <  4700) {
+        throw new Error(`New Error Less 4700: ${randNo}`);
     } else {
         await sleep(randNo);
     }
