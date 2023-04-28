9fc7d59294300c82910ea76d78b6dc270ef7b6ae
Hasitha Prabhath Gamage <hasithaishere@users.noreply.github.com>
2023-04-28 16:52:08 +0800

    Update app.js

--- service/scanner/app.js
+++ service/scanner/app.js
@@ -1,5 +1,6 @@
 // This is the mocked ECS service
 const http = require('http');
+// Add comment
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(req.url);
